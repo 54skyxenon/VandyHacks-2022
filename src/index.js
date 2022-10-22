f4f2e1f34f59523a6e2405fd6795b5d7dd2b9719
Brandon Liang <54skyxenon@gmail.com>
2022-10-22 16:33:28 -0500

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -10,7 +9,6 @@ import {
   Footer,
   Home,
   Upload,
-  Contact,
   Blog,
   Posts,
   Post,
